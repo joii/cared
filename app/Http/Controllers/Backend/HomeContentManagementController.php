dfa2ae63c311057a9924c5695c4fe8769a26bbfc
phatchara yubon <contact.puipui@gmail.com>
2025-07-14 11:44:15 +0700

    Update backend section Home content
    Update backend section Home content

--- app/Http/Controllers/Backend/HomeContentManagementController.php
+++ app/Http/Controllers/Backend/HomeContentManagementController.php
@@ -39,4 +39,46 @@ class HomeContentManagementController extends Controller
         return redirect()->route('backend.home_content.index')->with($notification,$contents);
     }//End Method
 
+     public function Edit(string $id)
+    {
+        $content = HomeContent::find($id);
+        return view('backend.home_content.edit', compact('content'));
+    }//End Method
+
+      public function Update(Request $request)
+    {
+
+        $content = HomeContent::find($request->id);
+        $content->name_en = $request->name_en;
+        $content->name_th = $request->name_th;
+        $content->detail_th = $request->detail_th;
+        $content->detail_en = $request->detail_en;
+        $content->external_url = $request->external_url;
+        $content->status = $request->status;
+        $content->save();
+
+
+         $notification = array(
+          'message' => 'Homepage content updated successfully',
+          'alert-type' => 'success'
+        );
+
+        $contents = HomeContent::all();
+        return redirect()->route('backend.home_content.index')->with($notification,$contents);
+    }//End Method
+
+         public function Destroy(string $id)
+    {
+        $content = HomeContent::find($id);
+        $content->delete();
+        $notification = array(
+          'message' => 'Home Content  deleted successfully',
+          'alert-type' => 'success'
+        );
+
+        $contents = HomeContent::all();
+        return redirect()->route('backend.home_content.index')->with($notification,$contents);
+
+    }//End Method
+
 }
